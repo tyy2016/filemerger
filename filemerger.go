344eb2720bb92981eb848cc60c6942bd9199c245
maxwell92 <710028463@qq.com>
2017-02-12 00:19:05 +0800

    filemerger

--- filemerger.go
+++ filemerger.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"os"
-	"fmt"
 	"filemerger"
+	"fmt"
+	"os"
 )
 
 func Usage() {
 	fmt.Println("Usage: filemerger [DIRECTORY]\n" +
-		       //TODO:"       filemerger [ --help | -v | --version ]\n\n" +
-		       "A very useful tool filemerger\n")
+		//TODO:"       filemerger [ --help | -v | --version ]\n\n" +
+		"A very useful tool filemerger\n")
 	os.Exit(1)
 }
 
@@ -18,6 +18,6 @@ func main() {
 		Usage()
 	}
 
-	fm := new(filemerger.FileMerger)
+	fm := filemerger.NewFileMerger(os.Args[1])
 	fm.Run()
 }
