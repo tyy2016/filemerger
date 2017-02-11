b4fde3180b2c0f51ff796f73ec535a8281aa86d9
maxwell92 <710028463@qq.com>
2017-02-11 19:06:31 +0800

    add filemerger

--- src/filemerger/filemerger.go
+++ src/filemerger/filemerger.go
@@ -1,1 +1,11 @@
 package filemerger
+
+import (
+	"fmt"
+)
+
+func (fp *FileMerger) Run() {
+	fmt.Println("FileMerger is Running")
+	log.Infof("FileMerger is Running")
+}
+
--- src/filemerger/types.go
+++ src/filemerger/types.go
@@ -1,1 +1,13 @@
 package filemerger
+
+import (
+	mylog "github.com/mlycore/tools/mylog"
+	"os"
+)
+
+var log =  mylog.NewLogger(os.Stderr, 3)
+
+type FileMerger struct {
+	Msg string
+}
+
