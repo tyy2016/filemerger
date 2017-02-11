69a6655d642bf4decd97c5d299d3cd879f433d67
tyy2016 <360398525@qq.com>
2017-02-12 00:17:05 +0800

    finish get filenamelist

--- src/fmutils/fmutils.go
+++ src/fmutils/fmutils.go
@@ -2,12 +2,27 @@ package fmutils
 
 import (
 	"fmt"
+	"io/ioutil"
 )
 
 func GetFilenameList(directory string) []string {
-	fmt.Println("this")
+
 	log.Infof("this is mushroom, %d", 1)
-	log.Errorf("error")
+
+	dirName, _ := ioutil.ReadDir(directory)
+	log.Infof("CurrentDir is %s",directory)
+
+
+	filenamelist := make([]string, 0)
+	for i := 0; i < len(dirName); i++ {
+		if dirName[i].IsDir() {
+
+		} else {
+			filename := dirName[i].Name()
+			filenamelist = append(filenamelist, filename)
+		}
+	}
+	fmt.Println("Filename_list:", filenamelist)
 
 	return nil
 }
